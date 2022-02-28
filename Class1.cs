dba348be2c4c348460b4e4f79e3c139cab51e53e
arnjok <arnjok@C406A3>
2022-02-28 10:05:31 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExClass
+{
+
+    class matematika
+    {
+        public double suskaiciuota = 1;
+        public double q = 2;//geopmetrines progresijos dauginamasis
+        public int k;//kiek kartu reikejo
+        public double iki;
+        private double temp=0;
+
+        public void uzduotis()
+        {
+            suskaiciuota = 1;
+            q = 2;
+            iki = Math.Pow(10, 15);
+            for (int i = 2; suskaiciuota < iki; i++)
+            {
+                temp = suskaiciuota;
+                suskaiciuota = suskaiciuota * q;
+                q = temp;
+                k = i;
+            }
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace ExClass 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime start;//kodo vykdymo pradžia
+            start = DateTime.Now;
+
+            DateTime end; //kodo vykdymo pabaiga
+
+            matematika obj = new matematika();
+            obj.uzduotis();
+
+            end = DateTime.Now;
+
+            TimeSpan laikas = (end - start);
+
+            Console.WriteLine("Galutinis skaicius: " + obj.suskaiciuota + "\nPrireikė kartų: " + obj.k + "\nUžtruko: " + laikas.TotalMilliseconds + " ms");
+            Console.ReadLine();
+        }
+    }
+}
